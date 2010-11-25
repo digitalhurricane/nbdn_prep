e6a13fa47747bb052716f10ee087a78bdad6f1e9
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-11-25 09:48:23 -0700

    Added rake back to path

--- build/build_utilities.rb
+++ build/build_utilities.rb
@@ -98,12 +98,9 @@ end
 
 class MSBuildRunner
 	def self.compile(attributes)
-		version = attributes.fetch(:clrversion, 'v3.5')
 		compile_target = attributes.fetch(:compile_target, 'debug')
-	    solution_file = attributes[:solution_file]
-		
-		framework_dir = File.join(ENV['WINDIR'].dup, 'Microsoft.NET', 'Framework', 'v3.5')
-		msbuild_file = File.join(framework_dir, 'msbuild.exe')
+    solution_file = attributes[:solution_file]
+		msbuild_file = 'xbuild'
 		
 		sh "#{msbuild_file} #{solution_file} /property:Configuration=#{compile_target} /t:Rebuild"
 	end
